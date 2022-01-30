7c7f3f6f73dc2ee936e402545ec58ccc911348a7
Nathan Burnett <bur19052@byui.edu>
2022-01-29 17:23:07 -0700

    Completed base game no UI
    Co-authored-by: Katzenbar02 <Katzenbar02@users.noreply.github.com>
    Co-authored-by: mmcconnehey <mmcconnehey@users.noreply.github.com>
    Co-authored-by: JoshuaCapron64 <JoshuaCapron64@users.noreply.github.com>
    Co-authored-by: Capron, Joshua <capronj@byui.edu>

---
+++ hilo/game/director.py
@@ -0,0 +1,104 @@
+from deck import Deck
+
+#Die == Deck
+#dice == card
+#roll == deal
+
+'''
+The player starts the game with 300 points.
+Individual cards are represented as a number from 1 to 13.
+The current card is displayed.
+The player guesses if the next one will be higher or lower.
+The the next card is displayed.
+The player earns 100 points if they guessed correctly.
+The player loses 75 points if they guessed incorrectly.
+If a player reaches 0 points the game is over.
+If a player has more than 0 points they decide if they want to keep playing.
+If a player decides not to play again the game is over.
+
+The card is: #
+Higher or lower? [h/l] x
+Next card was: #
+Your score is: ###
+Play again? [y/n] x
+'''
+
+class Director:
+    def __init__(self):
+        self.deal = []
+        self.is_playing = True
+        #self.score = 0
+        self.total_score = 300
+        self.first_round = False
+    #creates within a list two instances of deck class
+        for i in range(2):
+            deck = Deck()
+            self.deal.append(deck)
+
+    def start_game(self):
+        while self.is_playing:
+            self.play_game()
+            self.do_outputs()
+            self.play_again()
+    
+    def play_again(self):
+       draw_card = input('Do you want to play again? [y/n]:')
+       self.is_playing = (draw_card == 'y')
+       self.first_round = (draw_card == 'y')
+    
+    def play_game(self):
+        if not self.is_playing:
+            return
+
+        if self.first_round == False:
+            hand = self.deal[0]
+            hand.draw()
+            print(f'The card is: {hand.card}')
+            hand = hand.card
+
+            top_card = self.deal[1]
+            top_card.draw()
+        
+        elif self.first_round == True:
+            hand = self.deal[0]
+            print(f'The card is: {self.deal[0]}')
+            top_card = self.deal[1]
+            top_card.draw()
+
+        hi_lo = input('Higher or lower? [h/l]: ')
+
+            
+        if hi_lo == 'h':
+            if top_card.card <= hand:
+                #self.score += -75
+                self.total_score += -75
+
+            if top_card.card > hand:
+                #self.score += 100
+                self.total_score += 100
+
+        elif hi_lo == 'l':
+            if top_card.card >= hand:
+                #self.score += -75
+                self.total_score += -75
+            
+            if top_card.card < hand:
+                #self.score += 100
+                self.total_score += 100
+
+        print(f'The next card was: {top_card.card}')
+
+        self.deal[0] = top_card.card
+
+    
+    def do_outputs(self):
+        if not self.is_playing:
+            return
+        
+        print(f'Your score is: {self.total_score}')
+        self.is_playing == (self.total_score > 0)
+        print('')
+
+# director = Director()
+# director.start_game()
+
---
+++ hilo/readme.md
@@ -0,0 +1,39 @@
+# High/Low
+Would you like to play a game? Play cards and you might be surprised. The rules are simple. The
+Player draws a card, and you must guess if the next card has a higher or lower value than that
+initial card. Guess correctly and 100 points are added to your score! Guess incorrectly, and 75
+points are taken away. The game ends when you choose for it to end, or if your score reaches 0.
+
+## Getting Started
+---
+You will need Python 3.8.0 or newer installed and running on your machine. Open a terminal and 
+browse to the project's root folder. Start the program by running the following command.
+```
+python3 hilo 
+```
+You can also run the program from an IDE like Visual Studio Code. Start your IDE and open the 
+project folder. Select the main module inside the hilo folder and click the "run" button.
+
+## Project Structure
+---
+The project files and folders are organized as follows:
+```
+root                    (project root folder)
++-- hilo                (source code for game)
+  +-- game              (specific classes)
+    +-- __main__.py     (program entry point)
+    +-- deck.py
+    +-- director.py     
++-- README.md           (general info)
+```
+
+## Required Technologies
+---
+* Python 3.8.0
+
+## Authors
+---
+* Nathan Burnnett
+* Michael McConnehey
+* Joshua Capron
+* Joshua Ludwig
